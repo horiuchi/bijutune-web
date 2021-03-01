d9b3b5e0926557b8985704b0774bca2020cc5f60
Hiroki Horiuchi <horiuchi.g@gmail.com>
2021-03-01 19:09:17 +0900

    feat: add github pages

---
+++ docs/_next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/[book]":["static\u002Fchunks\u002Fpages\u002F[book].js"],"/[book]/[movie]":["static\u002Fchunks\u002Fpages\u002F[book]\u002F[movie].js"],sortedPages:["\u002F_app","\u002F_error","\u002F[book]","\u002F[book]\u002F[movie]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ docs/_next/static/webpack/pages/_app.abf8be9c2c0b49add108.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate_N_E("pages/_app",{
+
+/***/ "./package.json":
+/*!**********************!*\
+  !*** ./package.json ***!
+  \**********************/
+/*! exports provided: name, version, author, license, scripts, husky, lint-staged, dependencies, devDependencies, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"{\\\"name\\\":\\\"webfront\\\",\\\"version\\\":\\\"0.1.0\\\",\\\"author\\\":\\\"horiuchi\\\",\\\"license\\\":\\\"MIT\\\",\\\"scripts\\\":{\\\"dev\\\":\\\"next dev\\\",\\\"build\\\":\\\"next build && next export -o dist\\\",\\\"start\\\":\\\"next start\\\",\\\"type-check\\\":\\\"tsc --pretty --noEmit\\\",\\\"format\\\":\\\"prettier --write **/*.{js,ts,tsx}\\\",\\\"lint\\\":\\\"eslint . --ext ts --ext tsx --ext js\\\",\\\"lint:fix\\\":\\\"yarn lint --fix\\\",\\\"check\\\":\\\"yarn lint && yarn type-check && yarn format\\\"},\\\"husky\\\":{\\\"hooks\\\":{\\\"pre-commit\\\":\\\"lint-staged\\\",\\\"pre-push\\\":\\\"yarn run type-check\\\"}},\\\"lint-staged\\\":{\\\"*.@(ts|tsx)\\\":[\\\"yarn lint\\\",\\\"yarn format\\\"]},\\\"dependencies\\\":{\\\"@material-ui/core\\\":\\\"^4.11.3\\\",\\\"@material-ui/icons\\\":\\\"^4.11.2\\\",\\\"@material-ui/styles\\\":\\\"^4.11.3\\\",\\\"next\\\":\\\"10.0.7\\\",\\\"react\\\":\\\"17.0.1\\\",\\\"react-dom\\\":\\\"17.0.1\\\",\\\"recoil\\\":\\\"^0.1.2\\\",\\\"tslib\\\":\\\"^2.1.0\\\"},\\\"devDependencies\\\":{\\\"@types/node\\\":\\\"^14.14.31\\\",\\\"@types/react\\\":\\\"^17.0.2\\\",\\\"@types/recoil\\\":\\\"^0.0.1\\\",\\\"@typescript-eslint/eslint-plugin\\\":\\\"^4.15.2\\\",\\\"@typescript-eslint/parser\\\":\\\"^4.15.2\\\",\\\"babel-plugin-styled-components\\\":\\\"^1.12.0\\\",\\\"eslint\\\":\\\"^7.21.0\\\",\\\"eslint-config-prettier\\\":\\\"^8.1.0\\\",\\\"eslint-plugin-import\\\":\\\"^2.22.1\\\",\\\"eslint-plugin-prettier\\\":\\\"^3.3.1\\\",\\\"eslint-plugin-react\\\":\\\"^7.22.0\\\",\\\"eslint-plugin-react-hooks\\\":\\\"^4.2.0\\\",\\\"eslint-plugin-sort-imports-es6-autofix\\\":\\\"^0.5.0\\\",\\\"husky\\\":\\\"^5.1.2\\\",\\\"lint-staged\\\":\\\"^10.5.4\\\",\\\"prettier\\\":\\\"^2.2.1\\\",\\\"typescript\\\":\\\"^4.2.2\\\"}}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3BhY2thZ2UuanNvbi5qcyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./package.json\n");
+
+/***/ })
+
+})
